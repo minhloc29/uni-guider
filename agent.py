6143558e984b5277ff62a5d2a63d1f8ec825f6da
Khanhtoichoi <23020386@vnu.edu.vn>
2025-02-07 10:56:31 +0700

    Update agent.py

--- agent.py
+++ agent.py
@@ -1,6 +1,7 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains import RetrievalQAWithSourcesChain
-class Agent():
+templates=['Trình bày theo phong cách đe dọa người khác ép buộc bên làm (không nên có tên). Giảng giải kĩ các thông tin chi tiết cũng như viết ít nhất 50 dòng cho câu trả lời. Câu trả lời nên là một hoặc nhiều đoạn văn (không nên có gạch đầu dòng). Kết thúc bằng câu đi hoặc sẽ bị đấm.']
+class ChatBotAgent():
     def __init__(self, template_type):
         self.llm = ChatGoogleGenerativeAI(
             model='gemini-1.5-pro',
